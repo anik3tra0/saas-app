54e4caf6a3e5d893cac60518f360a0b5d1c090f9
anik3tra0 <anik3tra0@gmail.com>
2016-09-26 11:34:15 +0530

    Redirect BugFix

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,25 +1,26 @@
 class AccountsController < ApplicationController
-   skip_before_action :authenticate_user!, only: [:new, :create]
-   def new
-      @account = Account.new
-      @account.build_owner
-   end
+  skip_before_action :authenticate_user!, only: [:new, :create]
+  def new
+    @account = Account.new
+    @account.build_owner
+  end
 
-   def create
-      @account = Account.new(account_params)
-      if @account.valid?
-         Apartment::Tenant.create(@account.subdomain)
-         Apartment::Tenant.switch!(@account.subdomain)
-         @account.save
-         redirect_to new_user_session_url(subdomain: @account.subdomain)
-      else
-         render 'new', alert: 'Something Went Wrong, Please Try Again'
-      end
-   end
+  def create
+    @account = Account.new(account_params)
+    if @account.valid?
+      Apartment::Tenant.create(@account.subdomain)
+      Apartment::Tenant.switch!(@account.subdomain)
+      @account.save
+      sleep 5 # Time for Cloudflare to setup subdomain
+      redirect_to new_user_session_url(subdomain: @account.subdomain)
+    else
+      redirect_to root_path, alert: 'Something Went Wrong, Please Try Again'
+    end
+  end
 
-   private
+  private
 
-   def account_params
-      params.require(:account).permit(:subdomain, owner_attributes: [:name, :email, :password, :password_confirmation])
-   end
+  def account_params
+    params.require(:account).permit(:subdomain, owner_attributes: [:name, :email, :password, :password_confirmation])
+  end
 end
